6b07873fab19b1be329e1b01008c200450ed1921
KevinjCode <megbart@msn.com>
2025-10-16 15:53:00 +0200

    Ajout de la table

---
+++ tables.c
@@ -0,0 +1,16 @@
+const char* units[] = {
+    "zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"
+};
+
+const char* teens[] = {
+    "ten", "eleven", "twelve", "thirteen", "fourteen",
+    "fifteen", "sixteen", "seventeen", "eighteen", "nineteen"
+};
+
+const char* tens[] = {
+    "", "", "twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety"
+};
+
+const char* magnitudes[] = {
+    "", "thousand", "million", "billion", "trillion"
+};
