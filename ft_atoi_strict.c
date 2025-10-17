3a55ddbad74436c14f4e0ac101634f3eb04ce1bc
KevinjCode <megbart@msn.com>
2025-10-17 12:31:58 +0200

    Ajout atoi entier

--- ft_atoi_strict.c
+++ ft_atoi_strict.c
@@ -4,19 +4,19 @@ unsigned long	ft_atoi_strict(const char *str)
 {
 	int i;
 	unsigned long result;
-	int found;
 
 	result = 0;
 	i = 0;
-	found = 0;
 	while (str[i] == ' ' || str[i] == '\n' || str[i] == '\t' || str[i] == '\f')
 		i++;
 	if (str[i] == 0)
 		return(0);
 	while (str[i] >= '0' && str[i] <= '9')
 	{
-		found = 1;
 		result = result * 10 + (str[i] - '0');
 		i++;
 	}
-	while
+	if (str[i] != '\0')
+		return (0);
+	return (result);
+}
