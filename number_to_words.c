94dfac4cbdf5d76eaf7b94e7634ce7c4469700d9
KevinjCode <megbart@msn.com>
2025-10-17 16:08:42 +0200

    Esquisse de number_to_words

--- number_to_words.c
+++ number_to_words.c
@@ -1,8 +1,17 @@
 char	*number_to_words(unsigned long n)
 {
 	if(n == 0)
-	{
 		return ("Zero");
-	}
 	const char *magnitudes[] = {"", "thousand", "million", "billion", "trillion"};
-	
+
+	unsigned long *blocks;
+	int block_count;
+	int	i;
+
+
+	blocks = split_into_blocks();
+
+	while (i)
+		{
+			block_text = convert_block(bloks[i]));
+	}
